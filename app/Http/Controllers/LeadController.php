36ed7720cbdb96253970a29e12789e9454646377
gulshanlal <149589209+gulshanlal@users.noreply.github.com>
2024-04-12 18:42:15 +0530

    add Lead

---
+++ app/Http/Controllers/LeadController.php
@@ -0,0 +1,95 @@
+<?php
+
+namespace App\Http\Controllers;
+use App\Http\Controllers\LeadController;
+
+    
+use Illuminate\Http\Request;
+use App\Models\Lead;
+use DataTables;
+use Illuminate\Http\JsonResponse;
+
+class LeadController extends \App\Http\Controllers\Controller
+{
+    public function index(Request $request)
+    {
+        if ($request->ajax()) {
+            // $data = Lead::query();
+            $data = Lead::get();
+    
+            return DataTables::of($data)
+                    ->addIndexColumn()
+                    ->addColumn('action', function($row){
+                           $btn = '<a href="javascript:void(0)" data-toggle="tooltip"  data-id="'.$row->id.'" data-original-title="View" class="me-1 btn btn-info btn-sm showLead"><i class="fa-regular fa-eye"></i> View</a>';
+                           $btn .= '<a href="javascript:void(0)" data-toggle="tooltip"  data-id="'.$row->id.'" data-original-title="Edit" class="edit btn btn-primary btn-sm editLead"><i class="fa-regular fa-pen-to-square"></i> Edit</a>';
+                           $btn .= ' <a href="javascript:void(0)" data-toggle="tooltip"  data-id="'.$row->id.'" data-original-title="Delete" class="btn btn-danger btn-sm deleteLead"><i class="fa-solid fa-trash"></i> Delete</a>';
+                            return $btn;
+                    })
+                    ->rawColumns(['action'])
+                    ->make(true);
+        }
+          
+        return view('lead');
+    }
+         
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request): JsonResponse
+    {
+        $request->validate([
+            'name' => 'required',
+            'detail' => 'required',
+        ]);
+          
+        Lead::updateOrCreate([
+                    'id' => $request->product_id
+                ],
+                [
+                    'name' => $request->name, 
+                    'detail' => $request->detail
+                ]);        
+       
+        return response()->json(['success'=>'Lead saved successfully.']);
+    }
+  
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Lead  $lead
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id): JsonResponse
+    {
+        $lead = Lead::find($id);
+        return response()->json($lead);
+    }
+  
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Lead  $lead
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id): JsonResponse
+    {
+        $lead = Lead::find($id);
+        return response()->json($lead);
+    }
+      
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Lead  $lead
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id): JsonResponse
+    {
+        Lead::find($id)->delete();
+        
+        return response()->json(['success'=>'Lead deleted successfully.']);
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,9 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\LeadController;
+    
+Route::resource('lead', LeadController::class);
 
 Route::get('/', function () {
     return view('welcome');
