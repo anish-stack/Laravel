733de04ae8410623112368d33b247f501027ace4
gulshanlal <149589209+gulshanlal@users.noreply.github.com>
2024-04-19 18:00:38 +0530

    leadcallOutcome

--- app/Models/LeadCallOutCome.php
+++ app/Models/LeadCallOutCome.php
@@ -8,4 +8,12 @@ use Illuminate\Database\Eloquent\Model;
 class LeadCallOutCome extends Model
 {
     use HasFactory;
+
+    protected $primaryKey = 'lco_id';
+    protected $table = 'lead_call_out_comes';
+    protected $fillable = [
+        'lco_name', 'lco_status'
+    ];
+
 }
+
--- routes/web.php
+++ routes/web.php
@@ -2,8 +2,12 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\LeadController;
+use App\Http\Controllers\LeadCallOutComeController;
     
 Route::resource('lead', LeadController::class);
+Route::resource('leadcall', LeadCallOutComeController::class);
+
+Route::post('leadcall/update-status', [LeadCallOutComeController::class, 'updateStatus'])->name('leadcall.updateStatus');
 
 Route::get('/', function () {
     return view('welcome');
