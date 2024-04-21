754ca8bc22d75f66519f2d302614fcbae43e2f5b
gulshanlal <149589209+gulshanlal@users.noreply.github.com>
2024-04-21 13:41:13 +0530

    ignore

--- resources/views/LeadAdd/create.blade.php
+++ resources/views/LeadAdd/create.blade.php
@@ -36,23 +36,23 @@
                         <div class="form-group">
                             <label for="name" class="col-sm-2 control-label">Mobile:</label>
                             <div class="col-sm-12">
-                                <input type="text" class="form-control" id="name" name="name"
-                                    placeholder="Enter Name" value="" maxlength="50">
+                                <input type="text" class="form-control" id="mobile" name="mobile"
+                                    placeholder="Enter Mobile" value="" maxlength="50">
                                 <span id="name_error" class="error text-danger" style="display:none"></span>
                             </div>
                         </div>
                         <div class="form-group">
                             <label for="name" class="col-sm-2 control-label">Address:</label>
                             <div class="col-sm-12">
-                                <input type="text" class="form-control" id="name" name="name"
-                                    placeholder="Enter Name" value="" maxlength="50">
+                                <input type="text" class="form-control" id="address" name="address"
+                                    placeholder="Enter Address" value="" maxlength="50">
                                 <span id="name_error" class="error text-danger" style="display:none"></span>
                             </div>
                         </div>
                         <div class="form-group">
                             <label for="name" class="col-sm-2 control-label">City:</label>
                             <div class="col-sm-12">
-                                <input type="text" class="form-control" id="name" name="name"
+                                <input type="text" class="form-control" id="city" name="city"
                                     placeholder="Enter Name" value="" maxlength="50">
                                 <span id="name_error" class="error text-danger" style="display:none"></span>
                             </div>
@@ -60,24 +60,24 @@
                         <div class="form-group">
                             <label for="name" class="col-sm-4 control-label">Project Name:</label>
                             <div class="col-sm-12">
-                                <input type="text" class="form-control" id="name" name="name"
-                                    placeholder="Enter Name" value="" maxlength="50">
+                                <input type="text" class="form-control" id="project_name" name="project_name"
+                                    placeholder="Enter Project Name" value="" maxlength="50">
                                 <span id="name_error" class="error text-danger" style="display:none"></span>
                             </div>
                         </div>
                         <div class="form-group">
                             <label for="name" class="col-sm-4 control-label">Available Size:</label>
                             <div class="col-sm-12">
-                                <input type="text" class="form-control" id="name" name="name"
-                                    placeholder="Enter Name" value="" maxlength="50">
+                                <input type="text" class="form-control" id="a_size" name="a_size"
+                                    placeholder="Enter Available Size" value="" maxlength="50">
                                 <span id="name_error" class="error text-danger" style="display:none"></span>
                             </div>
                         </div>
                         <div class="form-group">
                             <label for="name" class="col-sm-2 control-label">Remarks:</label>
                             <div class="col-sm-6">
-                                <input type="text" class="form-control" id="name" name="name"
-                                    placeholder="Enter Name" value="" maxlength="50">
+                                <input type="text" class="form-control" id="remark" name="remark"
+                                    placeholder="Enter Remarks" value="" maxlength="50">
                                 <span id="name_error" class="error text-danger" style="display:none"></span>
                             </div>
                         </div>
